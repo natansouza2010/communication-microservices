ea6df9cf4a948865ad3c7db53bdba7d3ff70fe61
Natan Souza <natansouza2010@gmail.com>
2022-06-08 15:55:18 -0300

    second commit

---
+++ product-api-ms/src/main/java/br/com/productapims/ProductApiMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.productapims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApiMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApiMsApplication.class, args);
+	}
+
+}
---
+++ product-api-ms/src/test/java/br/com/productapims/ProductApiMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.productapims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
