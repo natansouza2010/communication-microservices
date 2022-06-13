e7d73bded3c5ca1a6201e7e6bbf2c008d37c3007
Natan Souza <natansouza2010@gmail.com>
2022-06-13 10:13:49 -0300

    Auth jwt

--- auth-ms/src/modules/user/service/UserService.js
+++ auth-ms/src/modules/user/service/UserService.js
@@ -2,6 +2,10 @@ import UserRepository from "../repository/UserRepository.js";
 import * as httpStatus from "../../../config/constants/httpStatus.js";
 import UserException from "../exception/UserException.js";
 
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
+import * as secrets from "../../../config/constants/secret.js"
+
 class UserService{
 
     async findByEmail(req){
@@ -40,6 +44,44 @@ class UserService{
     validateUserNotFound(user){
         if(!user){
             throw new UserException(httpStatus.BAD_REQUEST, 'User not found !')
+        }
+    }
+
+    async getAcessToken(req){
+        try{
+            const {email, password} = req.body;
+            this.validateAccessTokenData(email,password);
+            let user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validateAccessTokenData(password, user.password);
+            const authUser = {id : user.id , name: user.name, email : user.email}
+            const accessToken = jwt.sign({authUser}, secrets.API_SECRET ,{expiresIn: '1d'})
+            return {
+                status: httpStatus.SUCESS, 
+                accessToken: accessToken,
+                
+            }
+
+        }catch(err){
+            return {
+                status: err.status ? err.status : httpStatus.INTERNAL_SERVER_ERROR,
+                message: err.message,
+
+            }
+        }
+       
+    }
+
+    validateAccessTokenData(email, password){
+        if(!email || !password){
+            throw new UserException(httpStatus.UNAUTHORIZED, 'Email and password must be informed.')
+        }
+
+    }
+
+    async validatePassword(password, hashPassword){
+        if(!await bcrypt.compare(password, hashPassword)){
+            throw new UserException(httpStatus.UNAUTHORIZED, "Password doesn't match.")
         }
     }
 }
