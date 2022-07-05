a14bf3280547cfd56fd7f0cf76e8e7c275e52101
Natan Souza <natansouza2010@gmail.com>
2022-07-05 14:52:51 -0300

    Configura RabbitMq

--- product-api-ms/src/main/java/br/com/productapims/ProductApiMsApplication.java
+++ product-api-ms/src/main/java/br/com/productapims/ProductApiMsApplication.java
@@ -1,8 +1,13 @@
 package br.com.productapims;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.Entity;
+
+
+@EnableRabbit
 @SpringBootApplication
 public class ProductApiMsApplication {
 
