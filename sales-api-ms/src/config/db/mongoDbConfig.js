3387f27fea08b68a5fe3691eeb844d5b666a4043
Natan Souza <natansouza2010@gmail.com>
2022-07-11 10:25:48 -0300

    Conexão com MongoDB

---
+++ sales-api-ms/src/config/db/mongoDbConfig.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import {MONGO_DB_URL} from "./secrets/secrets.js"
+
+export function connect(){
+    mongoose.connect(MONGO_DB_URL, {
+        useNewUrlParser: true
+    })
+    mongoose.connection.on('connected', function(){
+        console.log('The application connected to mongoDB successfully!')
+    });
+    mongoose.connection.on('error', function(){
+        console.log('The application connected to mongoDB failed')
+    });
+}
