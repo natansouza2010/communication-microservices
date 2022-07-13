9c26d449b3747e1209e71606a1675a45cd15d4a3
Natan Souza <natansouza2010@gmail.com>
2022-07-13 15:29:54 -0300

    Config para aguardar o container do RabbitMQ subir

--- sales-api-ms/src/config/rabbitmq/rabbitConfig.js
+++ sales-api-ms/src/config/rabbitmq/rabbitConfig.js
@@ -10,25 +10,44 @@ import {
 import {RABBIT_MQ_URL} from '../constants/secrets.js';
 
 const HALF_SECOND = 500;
+const HALF_MINUTE = 3000;
+const CONTAINER_ENV = "container";
 
 export async function connectRabbitMq(){
+    const env = process.env.NODE_ENV;
+    console.log(env);
+    if(CONTAINER_ENV === env ){
+        console.log("Waiting for RabbitMQ to start...")
+        setInterval(()=>{
+            connectRabbitMqAndCreateQueues()
+        }, HALF_MINUTE)
+    }else{
+        connectRabbitMqAndCreateQueues();
+    }
+  
+
+
+}
+
+async function connectRabbitMqAndCreateQueues() {
     amqp.connect(RABBIT_MQ_URL, (error, connection)=>{
 
         if(error){
             throw error;
         }
+        console.log("Starting RabbitMQ...");
 
         createQueue(connection, PRODUCT_STOCK_UPDATE_QUEUE, PRODUCT_STOCK_UPDATE_ROUTING_KEY, PRODUCT_TOPIC);
         createQueue(connection, SALES_CONFIRMATION_QUEUE, SALES_CONFIRMATION_ROUTING_KEY, PRODUCT_TOPIC);
-        
+        console.log("Queues and Topics were defined.");
         setTimeout(function(){
             connection.close();
         }, HALF_SECOND)
     });
+    
+}
 
-   
-
-    function createQueue(connection, queue, routingKey, topic){
+function createQueue(connection, queue, routingKey, topic){
         connection.createChannel((error, channel) => {
             if(error){
                 throw error
@@ -46,4 +65,3 @@ export async function connectRabbitMq(){
         
     }
 
-}
